edc288808ddbebfb2dcda7441403e9992033bd19
Mehul Jain <bingemath08@gmail.com>
2026-04-01 20:54:25 +0530

    corrected test_tls_sni_basic

--- engine/tests/test_protocol_inspec.cpp
+++ engine/tests/test_protocol_inspec.cpp
@@ -4,24 +4,16 @@
 #include <vector>
 
 int main() {
-  std::vector<unsigned char> client_hello = {
-      0x16, 0x03, 0x01, 0x00, 0x3c, 0x01, 0x00, 0x00, 0x38, 0x03, 0x03};
+  std::vector<unsigned char> client_hello = {0x16, 0x03, 0x01, 0x00, 0x3c, 0x01,
+                                             0x00, 0x00, 0x38, 0x03, 0x03};
   client_hello.insert(client_hello.end(), 32, 0x00);
   client_hello.insert(client_hello.end(),
-                      {0x00, 0x00, 0x02, 0x13, 0x01, 0x01, 0x00, 0x00, 0x0d,
-                       0x00, 0x00, 0x00, 0x09, 0x00, 0x07, 0x00, 0x00, 0x04,
-                       0x74, 0x65, 0x73, 0x74});
+                      {0x00, 0x00, 0x02, 0x13, 0x01, 0x01, 0x00, 0x00,
+                       0x0d, 0x00, 0x00, 0x00, 0x09, 0x00, 0x07, 0x00,
+                       0x00, 0x04, 0x74, 0x65, 0x73, 0x74});
 
-  //auto sni = deepwire::protocol_inspec::extract_sni(client_hello.data(),
-                                                    // client_hello.size());
-  // assert(sni.has_value());
-  // assert(sni.value() == "test");
-
-  const std::vector<unsigned char> invalid_record = {0x15, 0x03, 0x03, 0x00,
-                                                     0x00};
-  auto missing = deepwire::protocol_inspec::extract_sni(invalid_record.data(),
-                                                        invalid_record.size());
-  assert(!missing.has_value());
-
-  return 0;
+  auto sni = deepwire::protocol_inspec::extract_sni(client_hello.data(),
+                                                    client_hello.size());
+  assert(sni.has_value());
+  assert(sni.value() == "test");
 }
